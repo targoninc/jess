656ba80a4db18588d2fc68178d5e65eb0782a5ae
alexa <35202909+loudar@users.noreply.github.com>
2025-06-22 22:03:55 +0200

    work on docs

--- docs/src/ui/components/sidebar-link.ts
+++ docs/src/ui/components/sidebar-link.ts
@@ -2,17 +2,20 @@ import type {PageInfo} from "../pageInfo.ts";
 import {type AnyElement, compute, create} from "@targoninc/jess/dist/src";
 import {currentPage} from "../state.ts";
 
+function toLink(title: string) {
+    return `#${title.toLowerCase().replace(/ /g, "-")}`;
+}
+
 export function sidebarLink(page: PageInfo): AnyElement {
     const isActive = compute(p => p === page.title, currentPage);
     const active = compute((a): string => a ? "active" : "_", isActive);
 
     return create("div")
-        .classes("sidebarLink", active)
         .children(
             create("a")
-                .classes("sidebar-link")
+                .classes("sidebar-link", active)
                 .text(page.title)
-                .href(`#${page.title.toLowerCase().replace(/ /g, "-")}`),
+                .href(toLink(page.title)),
             create("div")
                 .classes("sidebar-link-children")
                 .children(
--- docs/src/ui/state.ts
+++ docs/src/ui/state.ts
@@ -6,8 +6,19 @@ export const pages = signal<PageInfo[]>([]);
 export const currentPage = signal<string>("");
 export const currentPageContent = signal<string>("");
 
+function getCurrentPage() {
+    const url = new URL(window.location.href);
+    const searchParams = new URLSearchParams(url.search);
+    if (searchParams.has("page")) {
+        return searchParams.get("page") as string;
+    }
+
+    return "";
+}
+
 Api.getPages().then(newPages => {
     pages.value = newPages;
+    currentPage.value = getCurrentPage();
 });
 
 currentPage.subscribe(page => {
